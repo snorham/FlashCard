cc9be22e2f2e0c3386ac2c58d841bc34d4a5f7c6
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-17 10:58:06 -0500

    added handler and runnable to post splash

--- app/src/main/java/com/example/bfinerocks/flashcard/SplashActivity.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/SplashActivity.java
@@ -1,10 +1,28 @@
 package com.example.bfinerocks.flashcard;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by BFineRocks on 12/17/14.
  */
 public class SplashActivity extends Activity {
     private final int SPLASH_SCREEN_DISPLAY_TIME = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_SCREEN_DISPLAY_TIME);
+    }
 }
