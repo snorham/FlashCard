cec64dcda320c1d2ce628324642adfcb3fa82fe3
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-17 10:50:41 -0500

    created activity for splash

---
+++ app/src/main/java/com/example/bfinerocks/flashcard/SplashActivity.java
@@ -0,0 +1,10 @@
+package com.example.bfinerocks.flashcard;
+
+import android.app.Activity;
+
+/**
+ * Created by BFineRocks on 12/17/14.
+ */
+public class SplashActivity extends Activity {
+    private final int SPLASH_SCREEN_DISPLAY_TIME = 1000;
+}
