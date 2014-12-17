fb71633bcce788226b6fd9923eb9750a1f7e29b1
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-17 14:02:24 -0500

    created layout for homepage

---
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/HomepageFragment.java
@@ -0,0 +1,18 @@
+package com.example.bfinerocks.flashcard.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by BFineRocks on 12/17/14.
+ */
+public class HomepageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
