b0d8f0cc02702f115dc9deb7b56b337606af549c
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-17 14:08:35 -0500

    onCreateView in homepage frag with layout wired

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/HomepageFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/HomepageFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.bfinerocks.flashcard.R;
 
 /**
  * Created by BFineRocks on 12/17/14.
@@ -13,6 +17,11 @@ public class HomepageFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
+        TextView welcomeUser = (TextView) rootView.findViewById(R.id.homepage_welcome);
+        ListView listOfDecksSaved = (ListView) rootView.findViewById(R.id.homepage_listView);
+        TextView linkToCreateNewDeck = (TextView) rootView.findViewById(R.id.txt_create_new_deck);
+
+        return rootView;
     }
 }
