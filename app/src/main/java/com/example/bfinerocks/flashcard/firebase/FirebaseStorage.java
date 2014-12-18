94d5b907ca701a1326220f917bcc7ab9a17bc8bf
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-18 16:15:23 -0500

    added child event listener

--- app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
@@ -1,8 +1,10 @@
 package com.example.bfinerocks.flashcard.firebase;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -11,7 +13,9 @@ import java.util.Map;
 public class FirebaseStorage {
 
     private Firebase referenceWithUser = null;
-    private final static String DECK_LEVL_REF_KEY = "decks";
+    private Firebase referenceToUsersDeckLevel = null;
+    private final static String DECK_LEVL_REF_KEY = "study-shelf";
+
 
     public void createFirebaseReferenceWithUserNameForReference(String userName){
         Firebase refPriorToUser = new Firebase("https://dazzling-heat-5107.firebaseio.com/flash-cards/users");
@@ -22,8 +26,49 @@ public class FirebaseStorage {
         return referenceWithUser;
     }
 
-    public void addNewDeckToFirebaseUserReference(Map<String,Deck> deckOfCards){
-        Firebase deckLevelRef = getReferenceWithUser().child(DECK_LEVL_REF_KEY);
-        deckLevelRef.push().setValue(deckOfCards);
+    public void appendFirebaseReferenceWithDeckLevelReference(){
+        referenceToUsersDeckLevel = getReferenceWithUser().child(DECK_LEVL_REF_KEY);
+    }
+
+    public Firebase getReferenceToUsersDeckLevel(){
+        return referenceToUsersDeckLevel;
+    }
+
+    //the object in this map is a Deck object not yet create //todo create deck object
+    public void addNewDeckToFirebaseUserReference(Map<String,Object> deckOfCards){
+        getReferenceToUsersDeckLevel().push().setValue(deckOfCards);
+    }
+
+    public void getUsersDecksFromFirebase(){
+        getReferenceToUsersDeckLevel().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot != null){
+                    Map<String, Object> deckOfCards = (Map<String,Object>) dataSnapshot.getValue();
+                    StudyShelf myStudyShelf = new StudyShelf();
+                    myStudyShelf.add(deckOfCards);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
