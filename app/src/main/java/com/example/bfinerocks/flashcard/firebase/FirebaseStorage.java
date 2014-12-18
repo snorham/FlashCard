0b14eea599d79037051e768f18a4953d2fd32ec6
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-18 12:24:02 -0500

    created methods to create firebase reference with usersname and stored in variable

--- app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
@@ -1,11 +1,20 @@
 package com.example.bfinerocks.flashcard.firebase;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by BFineRocks on 12/17/14.
  */
 public class FirebaseStorage {
 
-    public void addUserReferenceToFirebase(String userName){
+    private Firebase referenceWithUser = null;
 
+    public void createFirebaseReferenceWithUserNameForReference(String userName){
+        Firebase refPriorToUser = new Firebase("https://dazzling-heat-5107.firebaseio.com/flash-cards/users");
+        referenceWithUser = refPriorToUser.child(userName);
+    }
+
+    public Firebase getReferenceWithUser(){
+        return referenceWithUser;
     }
 }
