514e2435a0bfe6e24ee880343f357f8e6e3a0a6b
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-18 15:53:31 -0500

    created method to add deck to firebase

--- app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/firebase/FirebaseStorage.java
@@ -2,19 +2,28 @@ package com.example.bfinerocks.flashcard.firebase;
 
 import com.firebase.client.Firebase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by BFineRocks on 12/17/14.
  */
 public class FirebaseStorage {
 
     private Firebase referenceWithUser = null;
+    private final static String DECK_LEVL_REF_KEY = "decks";
 
     public void createFirebaseReferenceWithUserNameForReference(String userName){
         Firebase refPriorToUser = new Firebase("https://dazzling-heat-5107.firebaseio.com/flash-cards/users");
-        referenceWithUser = refPriorToUser.child(userName).child("decks");
+        referenceWithUser = refPriorToUser.child(userName);
     }
 
     public Firebase getReferenceWithUser(){
         return referenceWithUser;
+    }
+
+    public void addNewDeckToFirebaseUserReference(Map<String,Deck> deckOfCards){
+        Firebase deckLevelRef = getReferenceWithUser().child(DECK_LEVL_REF_KEY);
+        deckLevelRef.push().setValue(deckOfCards);
     }
 }
