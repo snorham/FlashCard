7e61aada06ebf3008e205dfb6f3ee2d37aaa0af3
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-22 15:12:48 -0500

    added scroll view to create layout

--- app/src/main/java/com/example/bfinerocks/flashcard/dictionaryapi/DefinitionParsing.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/dictionaryapi/DefinitionParsing.java
@@ -9,9 +9,10 @@ import org.json.JSONObject;
  * Created by BFineRocks on 12/22/14.
  */
 public class DefinitionParsing {
-    //todo change to String return if working
-    public void parseDefinitionFromDictionaryAPI(JSONObject jsonObject) throws JSONException{
+
+    public String parseDefinitionFromDictionaryAPI(JSONObject jsonObject) throws JSONException{
         String wordDefinition = jsonObject.getString("text");
         Log.i("parsing", wordDefinition);
+        return wordDefinition;
     }
 }
---
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/CreateAndReviewFragment.java
@@ -0,0 +1,9 @@
+package com.example.bfinerocks.flashcard.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by BFineRocks on 12/22/14.
+ */
+public class CreateAndReviewFragment extends Fragment {
+}
