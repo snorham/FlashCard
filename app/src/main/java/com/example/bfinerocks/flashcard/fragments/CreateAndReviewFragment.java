80fed1038349516e140b65b6f83a5a50322b377c
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-23 12:03:47 -0500

    created model for wordcard

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/CreateAndReviewFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/CreateAndReviewFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.bfinerocks.flashcard.R;
 
@@ -13,6 +14,7 @@ import com.example.bfinerocks.flashcard.R;
  */
 public class CreateAndReviewFragment extends Fragment {
 
+
     public static CreateAndReviewFragment newInstance(){
         return new CreateAndReviewFragment();
     }
@@ -20,6 +22,7 @@ public class CreateAndReviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.create_and_review_fragment, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view);
         return rootView;
     }
 
