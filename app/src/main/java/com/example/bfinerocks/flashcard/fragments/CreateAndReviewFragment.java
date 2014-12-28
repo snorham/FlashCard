247d2b21c184091257c14357a26b395bc30f3727
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-28 17:05:41 -0500

    added deckname edit text to on create view

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/CreateAndReviewFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/CreateAndReviewFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.bfinerocks.flashcard.R;
@@ -22,6 +23,7 @@ public class CreateAndReviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.create_and_review_fragment, container, false);
+        EditText deckNameEditText = (EditText) rootView.findViewById(R.id.edtxt_list_title);
         ListView listView = (ListView) rootView.findViewById(R.id.list_view);
         return rootView;
     }
