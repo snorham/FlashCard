9df729896dfeb0298f922e78d12a58773b26f699
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-28 16:46:40 -0500

    made deck parcable

--- app/src/main/java/com/example/bfinerocks/flashcard/models/Deck.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/models/Deck.java
@@ -1,11 +1,13 @@
 package com.example.bfinerocks.flashcard.models;
 
+import android.os.Parcel;
+
 import java.util.List;
 
 /**
  * Created by BFineRocks on 12/28/14.
  */
-public class Deck {
+public class Deck implements android.os.Parcelable {
     private List<WordCard> myDeck;
     private String deckName;
 
@@ -28,4 +30,31 @@ public class Deck {
     public List<WordCard> getMyDeck(){
         return myDeck;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeTypedList(myDeck);
+        dest.writeString(this.deckName);
+    }
+
+    private Deck(Parcel in) {
+        in.readTypedList(myDeck, WordCard.CREATOR);
+        this.deckName = in.readString();
+    }
+
+    public static final Creator<Deck> CREATOR = new Creator<Deck>() {
+        public Deck createFromParcel(Parcel source) {
+            return new Deck(source);
+        }
+
+        public Deck[] newArray(int size) {
+            return new Deck[size];
+        }
+    };
 }
