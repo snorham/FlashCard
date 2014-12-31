62431a0a4d32c082ec97da3cc489691b77a247fc
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-30 19:06:12 -0500

    used view holder in adapter

--- app/src/main/java/com/example/bfinerocks/flashcard/adapters/DeckListCustomAdapter.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/adapters/DeckListCustomAdapter.java
@@ -1,9 +1,13 @@
 package com.example.bfinerocks.flashcard.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.example.bfinerocks.flashcard.R;
 import com.example.bfinerocks.flashcard.models.Deck;
 
 import java.util.List;
@@ -17,8 +21,31 @@ public class DeckListCustomAdapter extends ArrayAdapter<Deck> {
         TextView deckName;
     }
 
+    private int layoutResource;
+
     public DeckListCustomAdapter(Context context, int resource, List<Deck> objects) {
         super(context, resource, objects);
+        this.layoutResource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder();
+            holder.deckName = (TextView) convertView.findViewById(R.id.deck_name);
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Deck deck = getItem(position);
+        if(deck != null){
+            holder.deckName.setText(deck.getDeckName());
+        }
+        return convertView;
     }
 
 
