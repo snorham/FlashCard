c598c64516c23138a30e2f854514de6805be05a0
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-30 19:34:02 -0500

    created new activity to house flash card generator

---
+++ app/src/main/java/com/example/bfinerocks/flashcard/activities/WordCardGeneratorActivity.java
@@ -0,0 +1,9 @@
+package com.example.bfinerocks.flashcard.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by BFineRocks on 12/30/14.
+ */
+public class WordCardGeneratorActivity extends Activity {
+}
