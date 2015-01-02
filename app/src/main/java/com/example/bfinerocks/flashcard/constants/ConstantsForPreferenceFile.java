f1eeb350b75e0182695134b7dd9a21df2f24232b
Brandy Foster <brandy.foster@detroitlabs.com>
2015-01-02 18:04:49 -0500

    created pref constant file for keys

---
+++ app/src/main/java/com/example/bfinerocks/flashcard/constants/ConstantsForPreferenceFile.java
@@ -0,0 +1,12 @@
+package com.example.bfinerocks.flashcard.constants;
+
+/**
+ * Created by BFineRocks on 1/2/15.
+ */
+public class ConstantsForPreferenceFile {
+    public final static String PREF_AUTO_DEF_KEY = "auto_define_preference";
+    public final static String PREF_TEXT_COLOR_KEY ="text_color_preference";
+    public final static String PREF_BACKGROUND_COLOR_KEY ="background_color_preference"; //pref for background not set up yet
+    public final static String PREF_RANDOM_ORDER_KEY = "random_order_preference";
+
+}
