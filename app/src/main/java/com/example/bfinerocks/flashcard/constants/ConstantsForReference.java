a89ac3f83eb84db0f9719d8253dbf8e34c48a59a
Brandy Foster <brandy.foster@detroitlabs.com>
2015-01-04 18:08:32 -0500

    created new fragment for review, added static ref

--- app/src/main/java/com/example/bfinerocks/flashcard/constants/ConstantsForReference.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/constants/ConstantsForReference.java
@@ -6,4 +6,5 @@ package com.example.bfinerocks.flashcard.constants;
 public class ConstantsForReference {
     public static final String USER_FIREBASE_REFERENCE = "firebase_user_ref";
     public static final String USER_NAME_PREFERENCE = "preferredUser";
+    public static final String SELECTED_DECK_TO_REVIEW = "selectedDeck";
 }
