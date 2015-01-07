0745b61333481f8f5770a034ed10fe25c7822f76
Cory Gorham <cory.gorham@detroitlabs.com>
2015-01-07 13:32:06 -0500

    undoing change made in code that is unrelated to this bug

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/FlashCardGeneratorFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/FlashCardGeneratorFragment.java
@@ -54,11 +54,11 @@ public class FlashCardGeneratorFragment extends Fragment implements OnClickListe
         backButton.setOnClickListener(this);
         nextButton = (Button) rootView.findViewById(R.id.btn_next);
         nextButton.setOnClickListener(this);
+        displayNextCardOnScreen();
+        currentCard = 0;
         displayWordSide = true;
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         setCardDisplayRandom = sharedPreferences.getBoolean(ConstantsForPreferenceFile.PREF_RANDOM_ORDER_KEY, false);
-        currentCard = 0;
-        displayNextCardOnScreen();
         return rootView;
     }
 
