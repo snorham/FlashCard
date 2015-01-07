49e8e15f915eb620dadabab946895562b20b61bf
Brandy Foster <brandy.foster@detroitlabs.com>
2015-01-07 15:40:43 -0500

    disabled save button in create frag

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/HomepageFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/HomepageFragment.java
@@ -113,8 +113,7 @@ public class HomepageFragment extends Fragment implements OnClickListener, OnIte
             FirebaseStorage firebaseStorage = new FirebaseStorage(uiHandler);
             firebaseStorage.createFirebaseReferenceWithUserNameForReference(userReference);
             firebaseStorage.appendFirebaseReferenceWithDeckLevelReference();
-               firebaseStorage.getUsersDecksFromFirebase();
-
+            firebaseStorage.getUsersDecksFromFirebase();
         }
     }
 
