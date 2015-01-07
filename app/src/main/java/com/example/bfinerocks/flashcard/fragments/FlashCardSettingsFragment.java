c515dc941cf37769688284d1b0682f1670a3bff9
Cory Gorham <cory.gorham@detroitlabs.com>
2015-01-07 16:32:23 -0500

    adding up button navigation to settings fragment

--- app/src/main/java/com/example/bfinerocks/flashcard/fragments/FlashCardSettingsFragment.java
+++ app/src/main/java/com/example/bfinerocks/flashcard/fragments/FlashCardSettingsFragment.java
@@ -5,14 +5,41 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.bfinerocks.flashcard.R;
 import com.example.bfinerocks.flashcard.constants.ConstantsForPreferenceFile;
+import com.example.bfinerocks.flashcard.tools.FlashCardTools;
 
 /**
  * Created by BFineRocks on 1/2/15.
  */
 public class FlashCardSettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        FlashCardTools.enableNavigationalHomeButton(getActivity());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_settings:
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, new FlashCardSettingsFragment())
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            case android.R.id.home:
+                getActivity().finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
